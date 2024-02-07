71b3f0d3e20b3180e909b27093951bcfcd7c0c84
Alen Zgurić <zguric.alen@hotmail.com>
2024-02-07 22:44:00 +0100

    add ip, lon, lat data

---
+++ src/components/CatchIpAddress.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+function CatchIpAddress() {
+  const [ipAddress, setIpAddress] = useState("");
+  const [geoInfo, setGeoInfo] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchIPInfo = async () => {
+      try {
+        const response = await fetch(`http://ip-api.com/json/${ipAddress}`);
+        const data = await response.json();
+
+        setGeoInfo(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log("Failed to fetch IP:", error);
+      }
+    };
+
+    fetchIPInfo();
+    getVisitorIp();
+  }, [ipAddress]);
+
+  const getVisitorIp = async () => {
+    try {
+      const response = await fetch(`https://api.ipify.org`);
+      const data = await response.text();
+
+      setIpAddress(data);
+    } catch (error) {
+      console.log("Failed to fetch IP:", error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setIpAddress(e.target.value);
+  };
+
+  return (
+    <div className="CatchIpAddress">
+      <h3>Ip to location</h3>
+      {isLoading ? (
+        <div className="spinner-container">
+          <ClipLoader color={"aqua"} loading={isLoading} size={35} />
+        </div>
+      ) : (
+        <div className="form-area">
+          <input type="text" value={ipAddress} onChange={handleInputChange} />
+        </div>
+      )}
+
+      {geoInfo.country && (
+        <div>
+          <strong>Country:</strong>
+          {geoInfo.country} <br />
+          <strong>City:</strong>
+          {geoInfo.city} <br />
+          <strong>Latitude:</strong>
+          {geoInfo.lat} <br />
+          <strong>Longitude:</strong>
+          {geoInfo.lon} <br />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default CatchIpAddress;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import CatchIpAddress from '../components/CatchIpAddress'
+
+function HomePage() {
+  return (
+    <div className='HomePage'>
+        <div className="ip_address">
+            <CatchIpAddress/>
+        </div>
+        <div className="wheader">
+
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
